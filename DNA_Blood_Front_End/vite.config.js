bec52cfbe0df4f6d209ee719b89a1ad67f90add0
ThinhTTSE182004 <thinhttse182004@fpt.edu.vn>
2025-07-01 21:02:25 +0700

    Update CI/CD

--- DNA_Blood_Front_End/vite.config.js
+++ DNA_Blood_Front_End/vite.config.js
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+    allowedHosts: [
+      'all',
+      'broadband-powerseller-swim-floors.trycloudflare.com',
+      '.trycloudflare.com',
+    ],
+  },
   define: {
     global: 'window',
     'jQuery': 'window.jQuery',
     '$': 'window.jQuery'
   }
 });
-
