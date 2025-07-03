dbf196085aa7f78e6433fd3a3c91ca08fdac1742
ThinhTTSE182004 <thinhttse182004@fpt.edu.vn>
2025-07-03 15:50:45 +0700

    Modify lauchSetting

--- DNA_Blood_Front_End/vite.config.js
+++ DNA_Blood_Front_End/vite.config.js
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    strictPort: true,
-    allowedHosts: [
-      'all',
-      'broadband-powerseller-swim-floors.trycloudflare.com',
-      '.trycloudflare.com',
-    ],
-  },
   define: {
     global: 'window',
     'jQuery': 'window.jQuery',
