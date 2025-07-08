81b4ac0896b498e2916cc1708db02f7998d35fdc
DuyKhoi04 <khoind1235@gmail.com>
2025-07-09 01:44:22 +0700

    create api chatbot

---
+++ DNA_Blood_API/Controllers/ChatbotController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace YourNamespace.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatbotController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChatbotController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public class AskRequest
+        {
+            public string question { get; set; }
+        }
+
+        public class AskResponse
+        {
+            public string answer { get; set; }
+        }
+
+        [HttpPost("ask")]
+        public async Task<IActionResult> Ask([FromBody] AskRequest request)
+        {
+            var payload = new { question = request.question };
+            var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("http://localhost:8000/ask", content);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<AskResponse>(responseString);
+            return Ok(result);
+        }
+    }
+}
+
